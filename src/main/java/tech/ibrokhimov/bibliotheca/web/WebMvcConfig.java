c28d4859b7536de0978e5aa3f7b011ad280cb038
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2020-11-03 16:05:59 +0900

    Configure resource handler locations

---
+++ src/main/java/tech/ibrokhimov/bibliotheca/web/WebMvcConfig.java
@@ -0,0 +1,41 @@
+package tech.ibrokhimov.bibliotheca.web;
+
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.samskivert.mustache.Mustache;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("/resources/**")
+			.addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+	}
+	
+	@Bean
+	public Mustache.Compiler mustacheCompiler(Mustache.TemplateLoader mustacheTemplateLoader, Environment environment) {
+		
+		MustacheEnvironmentCollector collector = new MustacheEnvironmentCollector();
+		collector.setEnvironment(environment);
+		
+		return Mustache.compiler()
+				.defaultValue("")
+				.withDelims("{* *}")
+				.withLoader(mustacheTemplateLoader)
+				.withCollector(collector);
+	}
+	
+}
--- src/main/java/tech/ibrokhimov/bibliotheca/web/security/WebSecurityConfig.java
+++ src/main/java/tech/ibrokhimov/bibliotheca/web/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package tech.ibrokhimov.bibliotheca.web.security;
 
 import java.io.IOException;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +11,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import tech.ibrokhimov.bibliotheca.BibliothecaServer;
 import tech.ibrokhimov.bibliotheca.controller.Mappings;
@@ -33,6 +40,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(BibliothecaServer.getEncoder());
 	}
+	
+	
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -48,12 +62,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.loginProcessingUrl(String.format("%s/perform_login",Mappings.SIGN))
 				.defaultSuccessUrl(Mappings.HOME, true)
 				.failureHandler(authenticationFailureHandler())
+				.successHandler(authenticationSuccessHandler())
 				.and()
 			.logout()
 				.logoutUrl(String.format("%s/perform_logout",Mappings.SIGN))
+				.invalidateHttpSession(true)
 				.clearAuthentication(true)
 				.and()
 			.csrf().disable();
+		http.sessionManagement().maximumSessions(1).sessionRegistry(sessionRegistry());
+	}
+	
+	@Bean
+	public SessionRegistry sessionRegistry() {
+		return new SessionRegistryImpl();
+	}
+	
+	@Bean
+	public AuthenticationSuccessHandler authenticationSuccessHandler() {
+		return new AuthenticationSuccessHandler() {
+			
+			@Override
+			public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+					Authentication authentication) throws IOException, ServletException {
+				
+				Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+				
+				if(roles.contains("ROLE_ADMIN"))
+					response.sendRedirect(String.format("%s/dashboard", Mappings.ADMIN));
+				else
+					response.sendRedirect(Mappings.HOME);
+			}
+		};
 	}
 	
 	@Bean
